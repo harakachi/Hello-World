a856b69c7881abbdc5346cc7efb94eeeda116596
harakachi <harakachi@gmail.com>
2012-03-04 18:07:43 +0900

    test

--- public_html/index.rb
+++ public_html/index.rb
@@ -3,10 +3,10 @@
 print "Content-type: text/html\n\n"
 print "<HTML><BODY>\n"
 
-now = Time.now
+#now = Time.now
 print "Hello World"
 print "<br>\n"
-print now
+#print now
 print "</BODY></HTML>"
 
 exit
